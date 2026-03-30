fb1ae751a2ece9ccfe3027ff6447aae0c613cc52
Delwyn <178507206+nywled@users.noreply.github.com>
2026-03-30 19:46:44 +0800

    Update list command effect with pin

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -17,7 +17,7 @@ public class ListCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.showAllPersons();
+        model.showAllPersonsPinnedFirst();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
